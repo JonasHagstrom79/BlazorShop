68aa6c99c09a4b4db8e84ec83ed48baa9db238f0
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-02-19 14:58:53 +0100

    Initial push

--- BlazorShop/Shared/Product.cs
+++ BlazorShop/Shared/Product.cs
@@ -13,7 +13,7 @@ namespace BlazorShop.Shared
         public string Title { get; set; } = string.Empty; //To avoid null reference exeption
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
-        public decimal Price { get; set; }
+        public decimal Price { get; set; }//price
         #endregion
     }
 }
