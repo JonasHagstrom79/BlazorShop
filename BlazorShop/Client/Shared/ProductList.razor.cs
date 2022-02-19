ba9a9a7a86e95dccc1f2ac02316a028e9bea07b7
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-02-19 16:52:23 +0100

    Added list of mock products

---
+++ BlazorShop/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,38 @@
+﻿using BlazorShop.Shared;
+
+namespace BlazorShop.Client.Shared
+{
+    public class ProductListrazor
+    {
+        //Mock-products
+        #region List
+        private static List<Product> products = new List<Product> 
+        { 
+            new Product
+            {
+                Id = 1,
+                Title ="The Hitchhiker's Guide to the Galaxy",
+                Description ="The Hitchhiker's Guide to the Galaxy[note 1] (sometimes referred to as HG2G,[1] HHGTTG,[2] H2G2,[3] or tHGttG) is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including stage shows, novels, comic books, a 1981 TV series, a 1984 text-based computer game, and 2005 feature film. ",
+                ImageUrl ="https://en.wikipedia.org/wiki/File:H2G2_UK_front_cover.jpg",
+                Price = 9.99m
+            },
+            new Product
+            {
+                Id = 2,
+                Title ="Ivanhoe",
+                Description ="Ivanhoe: A Romance (/ˈaɪvənˌhoʊ/) by Walter Scott is a historical novel published in three volumes, in 1819, as one of the Waverley novels. At the time it was written, the novel represented a shift by Scott away from writing novels set in Scotland in the fairly recent past to England in the Middle Ages. Ivanhoe proved to be one of the best-known and most influential of Scott's novels. ",
+                ImageUrl ="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Ivanhoe_title_page.jpg/220px-Ivanhoe_title_page.jpg",
+                Price = 5.99m
+            },
+            new Product
+            {
+                Id = 3,
+                Title ="The Silmarillion",
+                Description ="The Silmarillion (Quenya: [silmaˈrilliɔn]) is a collection of mythopoeic stories by the English writer J. R. R. Tolkien, edited and published posthumously by his son Christopher Tolkien in 1977 with assistance from the fantasy author Guy Gavriel Kay.[T 1] The Silmarillion tells of Eä, a fictional universe that includes the Blessed Realm of Valinor, the once-great region of Beleriand, the sunken island of Númenor, and the continent of Middle-earth, where Tolkien's most popular works—The Hobbit and The Lord of the Rings—take place. ",
+                ImageUrl ="https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Silmarillion.png/220px-Silmarillion.png",
+                Price = 8.99m
+            }
+        };
+        #endregion
+    }
+}
