9acee03a9a95eb117d5e7accd4293533b676b410
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-02-20 11:32:19 +0100

    get call

--- BlazorShop/Server/Controllers/ProductController.cs
+++ BlazorShop/Server/Controllers/ProductController.cs
@@ -14,9 +14,9 @@ namespace BlazorShop.Server.Controllers
             new Product
             {
                 Id = 1,
-                Title ="The Hitchhiker's Guide to the GalaxyHejHå",
+                Title ="The Hitchhiker's Guide to the Galaxy",
                 Description ="The Hitchhiker's Guide to the Galaxy[note 1] (sometimes referred to as HG2G,[1] HHGTTG,[2] H2G2,[3] or tHGttG) is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including stage shows, novels, comic books, a 1981 TV series, a 1984 text-based computer game, and 2005 feature film. ",
-                ImageUrl ="https://en.wikipedia.org/wiki/File:H2G2_UK_front_cover.jpg",
+                ImageUrl ="https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
                 Price = 9.99m
             },
             new Product
