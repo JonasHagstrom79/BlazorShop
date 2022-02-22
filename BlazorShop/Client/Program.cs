7f06feb48a208480d7634b6ba2f7c131c1ff5aab
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-02-22 09:24:14 +0100

    Added CategoryService on the Client

--- BlazorShop/Client/Program.cs
+++ BlazorShop/Client/Program.cs
@@ -1,6 +1,7 @@
 global using BlazorShop.Shared;
 global using System.Net.Http.Json;
 global using BlazorShop.Client.Services.ProductService;
+global using BlazorShop.Client.Services.CategoryService;
 using BlazorShop.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>(); //for dependencyinjections and use of product && Iproduct
+builder.Services.AddScoped<ICategoryService, CategoryService>(); //As above, dont forget add global using @ top && _Import.razor
 
 await builder.Build().RunAsync();
