e033e97f6aa32553114c4726115c5c97b0366b95
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-02-23 16:50:33 +0100

    Add a category service to the client

---
+++ BlazorShop/Client/Services/CategorySerrvice/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShop.Client.Services.CategorySerrvice
+{
+    public interface ICategoryService
+    {
+        //acess the category from the service
+        List<Category> Categories{ get; set; }
+        Task GetCategories();
+    }
+}
