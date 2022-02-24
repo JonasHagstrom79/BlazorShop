8c8eb6f099d130c6d80697bc3a7c959e1cd14e8e
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-02-24 09:06:58 +0100

    Add a composite primary key & more seeding

--- BlazorShop/Server/Seed/ModelBuilderExtensions.cs
+++ BlazorShop/Server/Seed/ModelBuilderExtensions.cs
@@ -4,6 +4,23 @@
     {
         public static void Seed(this ModelBuilder modelBuilder) 
         {
+            //for the key
+            modelBuilder.Entity<ProductVariant>()
+                .HasKey(p => new { p.ProductId, p.ProductTypeId } );//Composite primary key
+            
+            modelBuilder.Entity<ProductType>().HasData(
+                    new ProductType { Id = 1, Name = "Default" },
+                    new ProductType { Id = 2, Name = "Paperback" },
+                    new ProductType { Id = 3, Name = "E-Book" },
+                    new ProductType { Id = 4, Name = "Audiobook" },
+                    new ProductType { Id = 5, Name = "Stream" },
+                    new ProductType { Id = 6, Name = "Blu-ray" },
+                    new ProductType { Id = 7, Name = "VHS" },
+                    new ProductType { Id = 8, Name = "PC" },
+                    new ProductType { Id = 9, Name = "PlayStation" },
+                    new ProductType { Id = 10, Name = "Xbox" }
+                );
+
             #region Category
             modelBuilder.Entity<Category>().HasData(
                 new Category
@@ -34,7 +51,6 @@
                     Title = "The Hitchhiker's Guide to the Galaxy",
                     Description = "The Hitchhiker's Guide to the Galaxy[note 1] (sometimes referred to as HG2G,[1] HHGTTG,[2] H2G2,[3] or tHGttG) is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including stage shows, novels, comic books, a 1981 TV series, a 1984 text-based computer game, and 2005 feature film. ",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
-                    Price = 9.99m,
                     CategoryId = 1                    
                 },
                 new Product
@@ -43,7 +59,6 @@
                     Title = "Ivanhoe",
                     Description = "Ivanhoe: A Romance (/ˈaɪvənˌhoʊ/) by Walter Scott is a historical novel published in three volumes, in 1819, as one of the Waverley novels. At the time it was written, the novel represented a shift by Scott away from writing novels set in Scotland in the fairly recent past to England in the Middle Ages. Ivanhoe proved to be one of the best-known and most influential of Scott's novels. ",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Ivanhoe_title_page.jpg/220px-Ivanhoe_title_page.jpg",
-                    Price = 5.99m,
                     CategoryId = 1
                 },
                 new Product
@@ -52,14 +67,12 @@
                     Title = "The Silmarillion",
                     Description = "The Silmarillion (Quenya: [silmaˈrilliɔn]) is a collection of mythopoeic stories by the English writer J. R. R. Tolkien, edited and published posthumously by his son Christopher Tolkien in 1977 with assistance from the fantasy author Guy Gavriel Kay.[T 1] The Silmarillion tells of Eä, a fictional universe that includes the Blessed Realm of Valinor, the once-great region of Beleriand, the sunken island of Númenor, and the continent of Middle-earth, where Tolkien's most popular works—The Hobbit and The Lord of the Rings—take place. ",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Silmarillion.png/220px-Silmarillion.png",
-                    Price = 8.99m,
                     CategoryId = 1
                 },
                 new Product
                     {
                         Id = 4,
                         CategoryId = 2,
-                        Price = 4.99m,
                         Title = "The Matrix",
                         Description = "The Matrix is a 1999 science fiction action film written and directed by the Wachowskis, and produced by Joel Silver. Starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano, and as the first installment in the Matrix franchise, it depicts a dystopian future in which humanity is unknowingly trapped inside a simulated reality, the Matrix, which intelligent machines have created to distract humans while using their bodies as an energy source. When computer programmer Thomas Anderson, under the hacker alias \"Neo\", uncovers the truth, he \"is drawn into a rebellion against the machines\" along with other people who have been freed from the Matrix.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
@@ -68,7 +81,6 @@
                     {
                         Id = 5,
                         CategoryId = 2,
-                        Price = 3.99m,
                         Title = "Back to the Future",
                         Description = "Back to the Future is a 1985 American science fiction film directed by Robert Zemeckis. Written by Zemeckis and Bob Gale, it stars Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, and Thomas F. Wilson. Set in 1985, the story follows Marty McFly (Fox), a teenager accidentally sent back to 1955 in a time-traveling DeLorean automobile built by his eccentric scientist friend Doctor Emmett \"Doc\" Brown (Lloyd). Trapped in the past, Marty inadvertently prevents his future parents' meeting—threatening his very existence—and is forced to reconcile the pair and somehow get back to the future.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
@@ -77,7 +89,6 @@
                     {
                         Id = 6,
                         CategoryId = 2,
-                        Price = 2.99m,
                         Title = "Toy Story",
                         Description = "Toy Story is a 1995 American computer-animated comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. The first installment in the Toy Story franchise, it was the first entirely computer-animated feature film, as well as the first feature film from Pixar. The film was directed by John Lasseter (in his feature directorial debut), and written by Joss Whedon, Andrew Stanton, Joel Cohen, and Alec Sokolow from a story by Lasseter, Stanton, Pete Docter, and Joe Ranft. The film features music by Randy Newman, was produced by Bonnie Arnold and Ralph Guggenheim, and was executive-produced by Steve Jobs and Edwin Catmull. The film features the voices of Tom Hanks, Tim Allen, Don Rickles, Wallace Shawn, John Ratzenberger, Jim Varney, Annie Potts, R. Lee Ermey, John Morris, Laurie Metcalf, and Erik von Detten. Taking place in a world where anthropomorphic toys come to life when humans are not present, the plot focuses on the relationship between an old-fashioned pull-string cowboy doll named Woody and an astronaut action figure, Buzz Lightyear, as they evolve from rivals competing for the affections of their owner, Andy Davis, to friends who work together to be reunited with Andy after being separated from him.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
@@ -88,7 +99,6 @@
                         Id = 7,
                         CategoryId = 3,
                         Title = "Half-Life 2",
-                        Price = 49.99m,
                         Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
 
@@ -98,7 +108,6 @@
                         Id = 8,
                         CategoryId = 3,
                         Title = "Diablo II",
-                        Price = 9.99m,
                         Description = "Diablo II is an action role-playing hack-and-slash computer video game developed by Blizzard North and published by Blizzard Entertainment in 2000 for Microsoft Windows, Classic Mac OS, and macOS.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png",
                     },
@@ -106,7 +115,6 @@
                     {
                         Id = 9,
                         CategoryId = 3,
-                        Price = 14.99m,
                         Title = "Day of the Tentacle",
                         Description = "Day of the Tentacle, also known as Maniac Mansion II: Day of the Tentacle, is a 1993 graphic adventure game developed and published by LucasArts. It is the sequel to the 1987 game Maniac Mansion.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg",
@@ -115,7 +123,6 @@
                     {
                         Id = 10,
                         CategoryId = 3,
-                        Price = 159.99m,
                         Title = "Xbox",
                         Description = "The Xbox is a home video game console and the first installment in the Xbox series of video game consoles manufactured by Microsoft.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox-console.jpg",
@@ -124,13 +131,125 @@
                     {
                         Id = 11,
                         CategoryId = 3,
-                        Price = 79.99m,
                         Title = "Super Nintendo Entertainment System",
                         Description = "The Super Nintendo Entertainment System (SNES), also known as the Super NES or Super Nintendo, is a 16-bit home video game console developed by Nintendo that was released in 1990 in Japan and South Korea.",
                         ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
                     }
                 );
             #endregion
+
+            modelBuilder.Entity<ProductVariant>().HasData(
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 2,
+                    Price = 9.99m,
+                    OriginalPrice = 19.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 3,
+                    Price = 7.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 4,
+                    Price = 19.99m,
+                    OriginalPrice = 29.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 2,
+                    ProductTypeId = 2,
+                    Price = 7.99m,
+                    OriginalPrice = 14.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 3,
+                    ProductTypeId = 2,
+                    Price = 6.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 5,
+                    Price = 3.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 6,
+                    Price = 9.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 7,
+                    Price = 19.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 5,
+                    ProductTypeId = 5,
+                    Price = 3.99m,
+                },
+                new ProductVariant
+                {
+                    ProductId = 6,
+                    ProductTypeId = 5,
+                    Price = 2.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 8,
+                    Price = 19.99m,
+                    OriginalPrice = 29.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 9,
+                    Price = 69.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 10,
+                    Price = 49.99m,
+                    OriginalPrice = 59.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 8,
+                    ProductTypeId = 8,
+                    Price = 9.99m,
+                    OriginalPrice = 24.99m,
+                },
+                new ProductVariant
+                {
+                    ProductId = 9,
+                    ProductTypeId = 8,
+                    Price = 14.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 10,
+                    ProductTypeId = 1,
+                    Price = 159.99m,
+                    OriginalPrice = 299m
+                },
+                new ProductVariant
+                {
+                    ProductId = 11,
+                    ProductTypeId = 1,
+                    Price = 79.99m,
+                    OriginalPrice = 399m
+                }
+            );
         }
     }
 }
