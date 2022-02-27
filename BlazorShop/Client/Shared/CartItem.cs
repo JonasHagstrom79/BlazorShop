96788e32d9f5b325d8b87eeb9ccdaa537e630d73
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-02-27 11:49:01 +0100

    Add CartItem class

---
+++ BlazorShop/Client/Shared/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShop.Client.Shared
+{
+    public class CartItem
+    {
+        //gets te info from the server
+        public int ProductId { get; set; }
+        public int ProductTypeId { get; set; }
+    }
+}
