03e59b79de5a715d192042d2c51714b129e571ba
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-03-07 08:31:31 +0100

    Add data annotations and validations

--- BlazorShop/Shared/UserRegister.cs
+++ BlazorShop/Shared/UserRegister.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace BlazorShop.Shared
 {
     public class UserRegister
     {
+        //Add data annotations, email is required for registration, also checks if its valid
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required, StringLength(100, MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
+        [Compare("Password", ErrorMessage = "The passwords do not match")]
         public string ComfirmPassword { get; set; } = string.Empty;
     }
 }
