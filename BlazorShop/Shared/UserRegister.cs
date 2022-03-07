41d775c22340029475ee5576b7a190fef4c3adb8
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-03-07 08:45:04 +0100

    Add validation to the Registration form

--- BlazorShop/Shared/UserRegister.cs
+++ BlazorShop/Shared/UserRegister.cs
@@ -10,9 +10,9 @@ namespace BlazorShop.Shared
     public class UserRegister
     {
         //Add data annotations, email is required for registration, also checks if its valid
-        [Required, EmailAddress]
+        [Required, EmailAddress(ErrorMessage = "E-postfältet är inte en giltig e-postadress")]
         public string Email { get; set; } = string.Empty;
-        [Required, StringLength(100, MinimumLength = 6)]
+        [Required, StringLength(100, MinimumLength = 6, ErrorMessage = "Lösenordet måste innehålla minst 6 tecken")]
         public string Password { get; set; } = string.Empty;
         [Compare("Password", ErrorMessage = "The passwords do not match")]
         public string ComfirmPassword { get; set; } = string.Empty;
