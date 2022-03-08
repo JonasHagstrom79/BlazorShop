7e8697bf4e1a3502614f54c9adce60f95489487f
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-03-08 10:56:18 +0100

    Update CategoryService.cs

--- BlazorShop/Client/Services/CategoryService/CategoryService.cs
+++ BlazorShop/Client/Services/CategoryService/CategoryService.cs
@@ -1,30 +1,24 @@
-﻿namespace BlazorShop.Client.Services.CategoryService
+namespace BlazorShop.Client.Services.CategorySerrvice
 {
     public class CategoryService : ICategoryService
     {
-        #region Properties
-        public List<Category> categories { get; set; }
         private readonly HttpClient _http;
-        #endregion
 
-        #region Constructor
-        public CategoryService(HttpClient http) //For the call
+        public List<Category> Categories { get; set; } = new List<Category>(); //Initilaze the categories as a new list
+
+        public CategoryService(HttpClient http)
         {
             _http = http;
         }
-        #endregion
-                
-        /// <summary>
-        /// Get a List of categories from the DB to the client
-        /// </summary>
-        /// <returns>List of categories</returns>
-        public async Task GetCategoriesAsync()
+
+        public async Task GetCategories()//Get the categories from serviceresponse JSon
         {
-            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
+            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category");
             if (response != null && response.Data != null)
             {
-                categories = response.Data;
-            }            
+                Categories = response.Data;
+            }
+            
         }
     }
 }
