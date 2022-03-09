d72830ee0725a525da26cbe69dc41d47b6324aee
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-03-09 07:46:36 +0100

    Add user login model

---
+++ BlazorShop/Shared/UserLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Shared
+{
+    public class UserLogin
+    {
+        //small class, only used for the request
+        [Required] //for the validation form
+        public string Email { get; set; } = string.Empty;
+        [Required] //for the validation form
+        public string Password { get; set; } = string.Empty;
+    }
+}
