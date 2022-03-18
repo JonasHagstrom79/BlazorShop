fcdd726f931843b13c7a2a13506ff5c526fdf348
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-03-18 10:57:06 +0100

    Create a success page and place an order

--- BlazorShop/Server/Services/PaymentService/PaymentService.cs
+++ BlazorShop/Server/Services/PaymentService/PaymentService.cs
@@ -9,15 +9,33 @@ namespace BlazorShop.Server.Services.PaymentService
         private readonly IAuthService _authService;
         private readonly IOrderService _orderService;
         private readonly IConfiguration _config; //for the API-KEY IMPORTANT!!!
+        
 
         //The Cart, Order and authentication for stripe
         public PaymentService(ICartService cartService, IAuthService authService, IOrderService orderService, IConfiguration config)
         {
-            StripeConfiguration.ApiKey = _config["ApiKey:StripeDevelop"];
+            //OnGet();
+            StripeConfiguration.ApiKey = "";
             _cartService = cartService;
             _authService = authService;
             _orderService = orderService;
             _config = config;
+        }
+
+        public void OnGet() 
+        {
+            var key2 = _config.GetSection("Appsettings: StripeApiKey").Value;
+            //string key = _config.GetConnectionString("Appsettings:StripeApiKey");
+            //String key1 = _config["Appsettings:StripeApiKey"].ToString();
+            //var key = _config.GetSection("c").ToString();
+            //var key = _config["Appsettings:StripeApiKey"].ToString();
+            //StripeConfiguration.ApiKey = key;
+            //StripeConfiguration.ApiKey = _config.GetSection("Appsettings:StripeApiKey").ToString();
+            //StripeConfiguration.ApiKey = $"{_config["Appsettings:StripeApiKey"]}";
+            //var key = 
+
+            //var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+            //    .GetBytes(_configuration.GetSection("AppSettings:Token").Value));
         }
 
         public async Task<Session> CreateCheckoutSession()
