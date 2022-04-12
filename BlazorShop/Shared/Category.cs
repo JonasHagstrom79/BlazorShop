f8cfdb7837dd375249823ebba3380e410c952c50
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-04-12 13:22:26 +0200

    Add more categories to the Category model

--- BlazorShop/Shared/Category.cs
+++ BlazorShop/Shared/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,17 @@ namespace BlazorShop.Shared
 {
     public class Category
     {
+        //Into the database = DTO
         #region Properties
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
+        public bool Visible { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+        [NotMapped] //Not a coulumn in the Database
+        public bool Editing { get; set; } = false;//Not in the database
+        [NotMapped]
+        public bool IsNew { get; set; } = false;//Not in the database
         #endregion
     }
 }
